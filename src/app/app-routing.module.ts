28d4d36b6e6151cd806a20af80cf32a3034df997
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2019-05-23 15:34:26 +0100

    create 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 import { AuthGuard } from './auth/auth.guard';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -14,11 +16,19 @@ const routes: Routes = [
     path: 'workout',
     loadChildren: './workout/workout.module#WorkoutModule',
     canLoad: [AuthGuard]
+  },
+  {
+    path: 'page-not-found',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'page-not-found'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), AuthRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SharedModule } from '../shared/shared.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [NavigationComponent, HomeComponent],
+  declarations: [NavigationComponent, HomeComponent, PageNotFoundComponent],
   imports: [LayoutModule, AppRoutingModule, SharedModule],
   exports: [NavigationComponent, AppRoutingModule]
 })
